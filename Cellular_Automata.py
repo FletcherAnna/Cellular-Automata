d2b55e521485ca0c17909b16524b19041d75af08
Anna Fletcher <fletcherannalynn@gmail.com>
2024-02-22 01:50:33 -0700

    Initial commit

---
+++ Cellular_Automata.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+
+
+
+
+
+def automaton(rule, size, steps):
+    
+    """Simulate the evolution of a cellular automaton
+    for a pre-determined number of steps.
+    
+    
+    Args:
+        rule (int): Identification of the update rule
+        size (int): Number of cells in each row
+        steps (int): Number of steps the automaton is allowed to progress
+        
+    Returns:
+        array[size:steps]
+    """
+    
+
+
+    return
+
+
+
+
+class Cell:
+    
+
+    """Representation of a single cell."""
+
+
